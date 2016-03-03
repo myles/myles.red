baa1cac23569337380c15acd885a6c7103029051
Myles Braithwaite <me@mylesbraithwaite.com>
2016-03-03 14:11:09 -0500

    Fixed some stuff up.

--- tasks/_aliases.js
+++ tasks/_aliases.js
@@ -1,28 +1,28 @@
 module.exports = function (grunt) {
-	grunt.registerTask('run', [
-		'jekyll:serve'
-	]);
-	
-	grunt.registerTask('build', [
-		'jekyll:build'
-	]);
-	
-	grunt.registerTask('deploy', [
-		'clean',
-		'jekyll:production',
-		's3:build',
-		'rsync:prod'
-	]);
-	
-	grunt.registerTask('staging', [
-		'jekyll:staging'
-	]);
-	
-	grunt.registerTask('push_podcasts', [
-		's3:podcasts'
-	]);
-	
-	grunt.registerTask('default', [
-		'run'
-	]);
+  grunt.registerTask('run', [
+    'jekyll:serve'
+  ]);
+
+  grunt.registerTask('build', [
+    'jekyll:build'
+  ]);
+
+  grunt.registerTask('deploy', [
+    'clean',
+    'jekyll:production',
+    'rsync:production',
+    's3:build'
+  ]);
+
+  grunt.registerTask('staging', [
+    'jekyll:staging'
+  ]);
+
+  grunt.registerTask('push_podcasts', [
+    's3:podcasts'
+  ]);
+
+  grunt.registerTask('default', [
+    'run'
+  ]);
 };
--- tasks/rsync.js
+++ tasks/rsync.js
@@ -1,19 +1,27 @@
 module.exports = function (grunt) {
-    var config = {
-        options: {
-					args: ["--verbose"],
-					recursive: true
-        },
-        prod: {
-        	options: {
-        		src: "./build/",
-        		dest: "/srv/www/myles.red/www/html",
-        		host: "bear",
-        		port: "22",
-        		delete: true,
-        	}
-        }
-    };
-    
-    grunt.config.set('rsync', config);
+  var config = {
+    options: {
+      args: ["--verbose"],
+      recursive: true
+    },
+    production: {
+      options: {
+        src: "./build/",
+        dest: "/srv/www/myles.red/www/html",
+        host: "bear",
+        port: "22",
+        delete: true,
+      }
+    },
+    podcasts: {
+      options: {
+        src: "./podcasts/",
+        dest: "/srv/www/myles.red/media/html/podcasts",
+        host: "bear",
+        port: "22"
+      }
+    }
+  };
+  
+  grunt.config.set('rsync', config);
 };
